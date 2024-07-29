602112e2c8ec436ae212ed791509b956ee70fa82
amresh-kumar-singh <kumar.akumar.amresh@gmail.com>
2024-07-29 20:50:04 +0530

    Feat: Coinpage candlestick chart and dark mode

---
+++ src/app/Components/ButtonGroup.tsx
@@ -0,0 +1,52 @@
+interface ItemType {
+  label: string;
+  value: string | number;
+}
+
+interface PropsTypes {
+  className: string;
+  onClick: (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    value: string | number
+  ) => void;
+  data: ItemType[];
+  selected: string | number;
+  disabled?: boolean;
+}
+
+export default function ButtonGroup({
+  className,
+  onClick,
+  disabled,
+  selected,
+  data,
+}: PropsTypes) {
+  return (
+    <div className={className}>
+      {data.map((item, index, arr) => {
+        let borderRound = "";
+        if (index == 0) {
+          borderRound = "border-t border-b border-l rounded-s-lg";
+        } else if (index == arr.length - 1) {
+          borderRound = "border rounded-e-lg";
+        } else {
+          borderRound = "border-t border-b border-l";
+        }
+        return (
+          <button
+            key={item.label}
+            disabled={selected === item.value}
+            onClick={(e) => onClick(e, item.value)}
+            className={`px-4 py-2 text-sm font-medium text-gray-900 border-gray-900 ${borderRound} hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-blue-700 dark:focus:bg-gray-700 ${
+              selected === item.value
+                ? "bg-gray-700 text-white"
+                : "bg-transparent"
+            }`}
+          >
+            {item.label}
+          </button>
+        );
+      })}
+    </div>
+  );
+}
---
+++ src/app/coins/[coin]/page.tsx
@@ -0,0 +1,114 @@
+"use client";
+import { fetchSingleCoin } from "@/app/actions";
+import { CoinType } from "@/app/Configs/types";
+import { useCryptoContext } from "@/app/Context/CryptoContext";
+import { useEffect, useState } from "react";
+import HTMLReactParser, { Element } from "html-react-parser";
+import Image from "next/image";
+import CoinInfo from "@/app/Components/CoinInfo";
+
+// export const generateMetadata = ({ params }: CoinPageType) => {
+//   retu
+// };
+
+type Coin = string;
+interface CoinPageType {
+  params: {
+    coin: Coin;
+  };
+}
+export default function Page({ params }: CoinPageType) {
+  const { coin } = params;
+  const [coinDetails, setCoinDetails] = useState<CoinType>();
+  const {
+    state: { symbol, currency, local, user, watchlist },
+  } = useCryptoContext();
+
+  useEffect(() => {
+    (async () => {
+      const data = await fetchSingleCoin(coin);
+      setCoinDetails(data);
+    })();
+  }, [coin]);
+
+  if (!coinDetails) return "Loading";
+  const inWatchlist = watchlist.includes(coinDetails?.id);
+
+  return (
+    <div className="containerRoot flex">
+      <title>{coin}</title>
+      <div className="sidebarRoot w-[35%] flex flex-col items-center m-6 border-r-2 border-gray-500">
+        <Image
+          src={coinDetails?.image.large}
+          alt={coinDetails?.name}
+          height="200"
+          width={200}
+          style={{ marginBottom: 20 }}
+        />
+        <h3 className="mb-2.5 text-3xl leading-tight tracking-normal font-bold lato">
+          {coinDetails.name}
+        </h3>
+        <p className="m-0 font-medium text-sm leading-[1.57] tracking-[0.00714em] w-full lato px-6 pb-3.5 text-justify">
+          {/* {coinDetails?.description.en} */}
+          {HTMLReactParser(String(coinDetails?.description.en.split("\n")[0]), {
+            replace: (node) => {
+              if (node instanceof Element && node.name) {
+                if (node.name === "a") {
+                  return node.children;
+                }
+              }
+            },
+          })}
+        </p>
+        <div className="self-start px-6 pt-2.5 pb-6.25">
+          <span className="flex">
+            <h6 className="marketDataRoot mb-2.5 text-xl leading-[1.6] tracking-[0.0075em] font-bold lato">
+              Rank:
+            </h6>
+            &nbsp; &nbsp;
+            <p className="m-0 font-normal text-base leading-[1.75] tracking-[0.00938em] lato">
+              {coinDetails?.market_cap_rank}
+            </p>
+          </span>
+
+          <span className="flex">
+            <h6 className="heading mb-2.5 text-xl leading-[1.6] tracking-[0.0075em] font-bold lato">
+              Current Price:
+            </h6>
+            &nbsp; &nbsp;
+            <p className="m-0 font-normal text-base leading-[1.75] tracking-[0.00938em] lato">
+              {symbol}{" "}
+              {
+                coinDetails.market_data.current_price[
+                  currency.toLowerCase() as keyof typeof coinDetails.market_data.market_cap
+                ].toLocaleString(local) // Solution to toLocaleString type issue at https://github.com/microsoft/TypeScript/pull/57679
+              }
+            </p>
+          </span>
+          <span className="flex ">
+            <h6 className="mb-2.5 text-xl leading-[1.6] tracking-[0.0075em] font-bold lato">
+              Market Cap:
+            </h6>
+            &nbsp; &nbsp;
+            <h6 className="flex m-0 font-normal text-base leading-[1.75] tracking-[0.00938em] lato">
+              {symbol}{" "}
+              {/* soln for below type issue https://stackoverflow.com/questions/57086672/element-implicitly-has-an-any-type-because-expression-of-type-string-cant-b */}
+              {coinDetails?.market_data.market_cap[
+                currency.toLowerCase() as keyof typeof coinDetails.market_data.market_cap
+              ]
+                .toLocaleString(local)
+                .slice(0, -7)}
+              M
+            </h6>
+          </span>
+          {user && (
+            <button>
+              {inWatchlist ? "Remove from Watchlist" : "Add to Watchlist"}
+            </button>
+          )}
+        </div>
+      </div>
+      <CoinInfo {...{ coin }} />
+    </div>
+  );
+}
